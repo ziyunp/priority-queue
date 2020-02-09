30a5ecf4ea6f0928bf99a83cd0ba40bc56017ad5
U-DESKTOP-75KQIKB\dkizh <dz819@ic.ac.uk>
2020-02-09 19:41:41 +0000

    feat: new class Tick

---
+++ src/simulation/Tick.java
@@ -0,0 +1,23 @@
+package simulation;
+
+public class Tick extends AbstractEvent {
+
+  /**
+   * Constructor for Tick.
+   *
+   * @param time
+   */
+  public Tick(double time) {
+    super(time);
+  }
+
+  @Override
+  public void happen(ParticleEventHandler h) {
+    h.reactTo(this);
+  }
+
+  @Override
+  public boolean isValid() {
+    return true;
+  }
+}

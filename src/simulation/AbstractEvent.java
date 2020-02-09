2e88d92f783d4fc67f2df6b3b68ecce26ec0dea0
U-DESKTOP-75KQIKB\dkizh <dz819@ic.ac.uk>
2020-02-09 19:46:59 +0000

    feat: AbstractEvent

--- src/simulation/AbstractEvent.java
+++ src/simulation/AbstractEvent.java
@@ -2,29 +2,33 @@ package simulation;
 
 public abstract class AbstractEvent implements Event {
 
-    /**
-     * Constructor for AbstractEvent.
-     */
-    public AbstractEvent(double time) {
-        // TODO implement the constructor 
-    }
+  private final double time;
 
-    /**
-     * Returns the time (in ticks) at which this event will occur.
-     */
-    @Override
-    public double time() {
-        // TODO implement this method
-        return 0;
-    }
+  /**
+   * Constructor for AbstractEvent.
+   */
+  public AbstractEvent(double time) {
+    this.time = time;
+  }
 
-    /**
-     * Compares this object with the specified Event.
-     */
-    @Override
-    public int compareTo(Event that) {
-        // TODO implement this method
-        return 0;
-    }
+  /**
+   * Returns the time (in ticks) at which this event will occur.
+   */
+  @Override
+  public double time() {
+    return time;
+  }
+
+  /**
+   * Compares this object with the specified Event.
+   */
+  @Override
+  public int compareTo(Event that) {
+    if (this.time < that.time())
+      return -1;
+    if (this.time > that.time())
+      return 1;
+    return 0;
+  }
 
 }

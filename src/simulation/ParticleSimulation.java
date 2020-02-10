eb0163023d916b7b61b89195981173f230868af5
U-DESKTOP-75KQIKB\dkizh <dz819@ic.ac.uk>
2020-02-10 17:26:11 +0000

    ParticleSimulation; runs but particles do not move

--- src/simulation/ParticleSimulation.java
+++ src/simulation/ParticleSimulation.java
@@ -11,12 +11,17 @@ public class ParticleSimulation implements Runnable, ParticleEventHandler {
 
   private final ParticlesModel model;
   private final ParticlesView screen;
+  private MinPriorityQueue<Event> queue;
+  private double clock = 0;
 
   /**
    * Constructor.
    */
   public ParticleSimulation(String name, ParticlesModel m) {
-    // TODO implement constructor
+    model = m;
+    screen = new ParticlesView(name, m);
+    queue = new MinPriorityQueue<>();
+    queue.add(new Tick(1));
   }
 
   /**
@@ -32,7 +37,33 @@ public class ParticleSimulation implements Runnable, ParticleEventHandler {
       e.printStackTrace();
     }
 
-    // TODO complete implementing this method
+    Event next = queue.remove();
+    if (next.isValid()) {
+      double dt = next.time() - clock;
+      clock = next.time();
+      model.moveParticles(dt);
+      next.happen(this);
+    }
   }
 
+
+  @Override
+  public void reactTo(Tick tick) {
+    try {
+      Thread.sleep(FRAME_INTERVAL_MILLIS);
+    } catch (InterruptedException e) {
+      return;
+    }
+    screen.update();
+    double nextTick = Math.ceil(clock);
+    queue.add(new Tick(nextTick));
+  }
+
+  @Override
+  public void reactTo(Collision c) {
+    Iterable<Collision> newCollisions = model.predictAllCollisions(clock);
+    for (Collision col: newCollisions) {
+      queue.add(col);
+    }
+  }
 }

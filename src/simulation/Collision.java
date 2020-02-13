c810a5b0774520ba1a439902382e2e991abea649
Ziyunp <ziyun_poh@hotmail.com>
2020-02-13 14:11:37 +0000

    remove getParticleHits()

--- src/simulation/Collision.java
+++ src/simulation/Collision.java
@@ -9,26 +9,23 @@ public abstract class Collision extends AbstractEvent {
   public Collision(double t, Particle[] ps) {
     super(t);
     particles = ps;
-    particleHits = getParticleHits();
+    for (Particle p : particles) {
+      particleHits += p.collisions();
+    }
   }
 
   /** Returns true if this Collision is (still) valid. */
   @Override
   public boolean isValid() {
-    int curHits = getParticleHits();
+    int curHits = 0;
+    for (Particle p : particles) {
+      curHits += p.collisions();
+    }
     return curHits == particleHits;
   }
 
   /** Returns an array containing the Particles involved in this Collision. */
   public Particle[] getParticles() {
     return particles;
-  }
-
-  private int getParticleHits() {
-    int hits = 0;
-    for (Particle p : particles) {
-      hits += p.collisions();
-    }
-    return hits;
   }
 }

06cdc41c042a1f9391a42ebc48409191f0d62af6
Ziyunp <ziyun_poh@hotmail.com>
2020-02-13 11:47:53 +0000

    refactor Collision

--- src/simulation/Collision.java
+++ src/simulation/Collision.java
@@ -3,36 +3,32 @@ package simulation;
 public abstract class Collision extends AbstractEvent {
 
   private final Particle[] particles;
-  int particleHits;
+  private int particleHits;
 
-  /**
-   * Constructor for Collision
-   */
+  /** Constructor for Collision */
   public Collision(double t, Particle[] ps) {
     super(t);
-    this.particles = ps;
-    particleHits = 0;
-    for (int i = 0; i < ps.length; i++) {
-      particleHits += ps[i].collisions();
-    }
+    particles = ps;
+    particleHits = getParticleHits();
   }
 
-  /**
-   * Returns true if this Collision is (still) valid.
-   */
+  /** Returns true if this Collision is (still) valid. */
   @Override
   public boolean isValid() {
-    int curHits = 0;
-    for (int i = 0; i < particles.length; i++) {
-      curHits += particles[i].collisions();
-    }
+    int curHits = getParticleHits();
     return curHits == particleHits;
   }
 
-  /**
-   * Returns an array containing the Particles involved in this Collision.
-   */
+  /** Returns an array containing the Particles involved in this Collision. */
   public Particle[] getParticles() {
     return particles;
+  }
+
+  private int getParticleHits() {
+    int hits = 0;
+    for (Particle p : particles) {
+      hits += p.collisions();
+    }
+    return hits;
   }
 }

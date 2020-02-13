b0a3b2a45688d40bc2088d25deea90b54722f202
Ziyunp <ziyun_poh@hotmail.com>
2020-02-13 12:05:14 +0000

    Apply google-java-format

--- src/simulation/AbstractEvent.java
+++ src/simulation/AbstractEvent.java
@@ -4,31 +4,22 @@ public abstract class AbstractEvent implements Event {
 
   private final double time;
 
-  /**
-   * Constructor for AbstractEvent.
-   */
+  /** Constructor for AbstractEvent. */
   public AbstractEvent(double time) {
     this.time = time;
   }
 
-  /**
-   * Returns the time (in ticks) at which this event will occur.
-   */
+  /** Returns the time (in ticks) at which this event will occur. */
   @Override
   public double time() {
     return time;
   }
 
-  /**
-   * Compares this object with the specified Event.
-   */
+  /** Compares this object with the specified Event. */
   @Override
   public int compareTo(Event that) {
-    if (this.time < that.time())
-      return -1;
-    if (this.time > that.time())
-      return 1;
+    if (this.time < that.time()) return -1;
+    if (this.time > that.time()) return 1;
     return 0;
   }
-
 }
